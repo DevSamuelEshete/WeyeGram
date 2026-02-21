d18409cce17a8545d64d43044ca2a13c64ee93fd
--replace-all <=>
2026-02-21 05:30:39 +0300

    Fixed SocektIo Corse Origin issue

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -34,7 +34,7 @@ const redisStore = new RedisStore({ client });
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: `http://localhost:${PORT}`,
     credentials: true,
   }),
 );
